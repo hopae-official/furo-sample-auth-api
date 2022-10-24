a5efdfb9992a569bcbafe4d216d26203cede2b76
Gavin Kim <zustkeeper@gmail.com>
2022-10-24 18:32:44 +0900

    Add comment for example

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,17 @@ router.get('/sign', function (req, res, next) {
   var options = req.body.options;
   if (!payload) payload = {};
   if (!options) options = {};
+
   // https://github.com/auth0/node-jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
+  // handle payload
+  /**
+  payload = { name: 'John Doe', uid: 'a0d53af8-5ce1-4f6f-a904-111ba7e1d61a' };
+  **/
+
+  // handle payload
+  /**
+  options = { expiresIn: '2h' };
+  **/
   var token = jwt.sign(payload, config.PRIVATE_KEY, options);
   res.send(token);
 });
@@ -34,6 +44,10 @@ router.get('/verify', function (req, res, next) {
       // https://github.com/auth0/node-jsonwebtoken#errors--codes
       next({ message: `jwt verify failed - [${err.name}] ${err.message}` });
     }
+    // handle decoded jwt
+    /**
+    console.log(decoded);
+    **/
     res.json(decoded);
   });
 });
